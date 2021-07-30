c683b326d90b0a53dc0ca2addd951a36aa0c357e
mojahige <mojahige@gmail.com>
2021-07-31 00:31:12 +0900

    😎 Hello Fastify!

---
+++ server/main.js
@@ -0,0 +1,20 @@
+require('make-promises-safe');
+
+const fastify = require('fastify')({
+  logger: true
+});
+
+fastify.get('/', async (_request, _reply) => {
+  return { hello: 'world' };
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
